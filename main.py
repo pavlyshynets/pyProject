3d68e7b0b40e3890823ff0c51fe6be373f90801d
pavlyshynets <mykola.pavlyshne@gmail.com>
2023-11-02 09:59:36 +0200

    finalProject

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from flask import Flask
+from waitress import serve
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
+@app.route('/api/v1/hello-world-17')
+def hello_world():
+    return 'Hello World 17', 200
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# if __name__ == '__main__':
+    # app.run(debug=True)
 
+serve(app, host='0.0.0.0', port=8000)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# http://127.0.0.1:5000/api/v1/hello-world-17
+# http://127.0.0.1:8000/api/v1/hello-world-17
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# waitress-serve --port=8000 main.py:app
